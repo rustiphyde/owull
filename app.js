8c70fd40408b0686c513a243f21da15074fd9077
Rusty Hoppins <rustiphyde@gmail.com>
2019-05-31 21:18:48 -0500

    Fix function

--- app.js
+++ app.js
@@ -45,7 +45,7 @@
     });
 
     //Add realtime authentication listener
-    firebase.auth().onAuthStateChange(firebaseUser => {
+    firebase.auth().onAuthStateChanged(firebaseUser => {
       if(firebaseUser) {
         console.log(firebaseUser);
       }
