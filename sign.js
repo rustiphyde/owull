e0efe5af7f8f36388c79f12e1b9d3c93b7b042cc
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-02 01:16:51 -0500

    Fix extra declaration issue

--- sign.js
+++ sign.js
@@ -1,54 +1,38 @@
-(function(){
-    const config = {
-        'apiKey': 'AIzaSyCsjfvMICsJGDkgbMfau_LVi_mnFEWUkU8',
-        'authDomain': 'owull-3714.firebaseapp.com',
-        'databaseURL': 'https://owull-3714.firebaseio.com',
-        'projectId': 'owull-3714',
-        'storageBucket': 'owull-3714.appspot.com',
-        'messagingSenderId': '481614411046',
-        'appId': '1:481614411046:web:27a15a56e4bf80d7'
-    };
-    // Initialize Firebase
+const btnSignUpCreate = document.getElementById('btnSignUp-create');
+const createEmail = document.getElementById('createEmail');
+const createPassword = document.getElementById('createPassword');
+const confirmPassword = document.getElementById('confirmPassword');
 
-    firebase.initializeApp(config);
+// Add login event
 
+// Add sign up event
+btnSignUpCreate.addEventListener('click', (e) => {
+    const cemail = createEmail.value;
+    const cpass = createPassword.value;
+    const confrmPass = confirmPassword.value;
+    const auth = firebase.auth();
+    // sign in
 
-    const btnSignUpCreate = document.getElementById('btnSignUp-create');
-    const createEmail = document.getElementById('createEmail');
-    const createPassword = document.getElementById('createPassword');
-    const confirmPassword = document.getElementById('confirmPassword');
+    if(confrmPass === cpass){
+        const promise = auth.createUserWithEmailAndPassword(cemail, cpass);
 
-    // Add login event
-
-    // Add sign up event
-    btnSignUpCreate.addEventListener('click', (e) => {
-        const cemail = createEmail.value;
-        const cpass = createPassword.value;
-        const confrmPass = confirmPassword.value;
-        const auth = firebase.auth();
-        // sign in
-
-        if(confrmPass === cpass){
-            const promise = auth.createUserWithEmailAndPassword(cemail, cpass);
-
-            promise.catch((e) => (
-                (document.querySelector(
-                    '.errormessg'
-                ).textContent = `Sign Up Unsuccessful.
+        promise.catch((e) => (
+            (document.querySelector(
+                '.errormessg'
+            ).textContent = `Sign Up Unsuccessful.
       Please try again.`),
-                console.log(e.message)
-            ));
-        }
-        else{
-            return (
-                (document.querySelector(
-                    '.errormessg'
-                ).textContent = `Passwords don't match.
+            console.log(e.message)
+        ));
+    }
+    else{
+        return (
+            (document.querySelector(
+                '.errormessg'
+            ).textContent = `Passwords don't match.
       Please try again.`),
-                console.log(e.message)
-            );
-        }
-    })
-    ;
- // eslint-disable-next-line indent
- }());
+            console.log(e.message)
+        );
+    }
+});
+// eslint-disable-next-line indent
+
