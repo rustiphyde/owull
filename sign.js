2d1e10ab73c400f952cf0a5dcbc23da0c1577fda
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-02 02:04:17 -0500

    Adjust sign up page

--- sign.js
+++ sign.js
@@ -1,38 +1,26 @@
 const btnSignUpCreate = document.getElementById('btnSignUp-create');
 const createEmail = document.getElementById('createEmail');
 const createPassword = document.getElementById('createPassword');
-const confirmPassword = document.getElementById('confirmPassword');
+
 
 // Add login event
 
 // Add sign up event
 btnSignUpCreate.addEventListener('click', (e) => {
-    const cemail = createEmail.value;
-    const cpass = createPassword.value;
-    const confrmPass = confirmPassword.value;
+    const email = createEmail.value;
+    const pass = createPassword.value;
     const auth = firebase.auth();
     // sign in
 
-    if(confrmPass === cpass){
-        const promise = auth.createUserWithEmailAndPassword(cemail, cpass);
+   const promise = auth.createUserWithEmailAndPassword(email, pass);
 
-        promise.catch((e) => (
-            (document.querySelector(
+        promise.catch( e => {
+          return document.querySelector(
                 '.errormessg'
             ).textContent = `Sign Up Unsuccessful.
-      Please try again.`),
-            console.log(e.message)
-        ));
-    }
-    else{
-        return (
-            (document.querySelector(
-                '.errormessg'
-            ).textContent = `Passwords don't match.
-      Please try again.`),
-            console.log(e.message)
-        );
-    }
-});
+      Please try again.`,
+            console.log(e.message);
+          });
+    });
 // eslint-disable-next-line indent
 
