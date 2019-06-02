75c9d28dbd570f4b2e96b942494f7770e9ce8489
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-02 01:12:44 -0500

    Redo fxns

---
+++ app.js
@@ -0,0 +1,34 @@
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+
+btnLogin.addEventListener('click', (e) => {
+    // get email and password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch( e => {
+        return document.querySelector('.errormessg').textContent = `Email and/or Password invalid. Please try again.`,
+        console.log(e.message)});
+});
+
+
+btnSignUp.addEventListener('click', e => {
+    location.href = "./signup.html", console.log(e.message);
+});
+
+btntempLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+// Add realtime authentication listener
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+    if(firebaseUser){
+        console.log(firebaseUser);
+    }
+    else{
+        console.log('You are now logged out.');
+    }
+});
