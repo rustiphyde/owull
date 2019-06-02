119e524e44bdfb6ee7bf1432f7054bafdcd0820b
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-02 01:24:20 -0500

    Clean up function code

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUp = document.getElementById('btnSignUp');
+const btntempLogout = document.getElementById('btnTempLogout');
 
 btnLogin.addEventListener('click', (e) => {
     // get email and password
     const email = txtEmail.value;
     const pass = txtPassword.value;
+    // eslint-disable-next-line no-undef
     const auth = firebase.auth();
     // sign in
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch( e => {
-        return document.querySelector('.errormessg').textContent = `Email and/or Password invalid. Please try again.`,
-        console.log(e.message)});
+
+    promise.catch((e) => document.querySelector('.errormessg').textContent = 'Email and/or Password invalid. Please try again.',
+        console.log(e.message));
 });
 
 
-btnSignUp.addEventListener('click', e => {
-    location.href = "./signup.html", console.log(e.message);
+btnSignUp.addEventListener('click', (e) => {
+    location.href = './signup.html', console.log(e.message);
 });
 
-btntempLogout.addEventListener('click', e => {
+// eslint-disable-next-line no-undef
+btntempLogout.addEventListener('click', (e) => {
     firebase.auth().signOut();
 });
 // Add realtime authentication listener
+// eslint-disable-next-line no-undef
 firebase.auth().onAuthStateChanged((firebaseUser) => {
     if(firebaseUser){
         console.log(firebaseUser);
