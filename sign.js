10d4a07ae68b6128c5db938ce80600f14432a1d0
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-03 01:54:32 -0500

    Add Login Redirect

--- sign.js
+++ sign.js
@@ -1,26 +1,28 @@
 const btnSignUpCreate = document.getElementById('btnSignUp-create');
 const createEmail = document.getElementById('createEmail');
 const createPassword = document.getElementById('createPassword');
+const btnLoginRedirect = document.getElementById('btnLoginRedirect');
 
+// Login Redirect
 
-// Add login event
+btnLoginRedirect.addEventListener((e) => {
+    location.href = './index.html';
+});
 
-// Add sign up event
+// // Add sign up event
 btnSignUpCreate.addEventListener('click', (e) => {
     const email = createEmail.value;
     const pass = createPassword.value;
     const auth = firebase.auth();
     // sign in
 
-   const promise = auth.createUserWithEmailAndPassword(email, pass);
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
 
-        promise.catch( e => {
-          return document.querySelector(
+    promise.catch((e) => document.querySelector(
                 '.errormessg'
             ).textContent = `Sign Up Unsuccessful.
       Please try again.`,
-            console.log(e.message);
-          });
-    });
+            console.log(e.message));
+});
 // eslint-disable-next-line indent
 
