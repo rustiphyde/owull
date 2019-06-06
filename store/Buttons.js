9656745fa3834bbf018bfb7fec831aa735380a2c
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-06 11:44:54 -0500

    Create Buttons file in store for modular Button creation

---
+++ store/Buttons.js
@@ -0,0 +1,8 @@
+import * as fxns from './../util';
+
+export default {
+    'fxnal': [ { 'fxn': fxns.login, 'text': 'LOGIN' }, { 'fxn': fxns.signup, 'text': 'SIGN UP' }, { 'fxn': fxns.reset, 'text': 'RESET' }, { 'fxn': fxns.poosh, 'text': 'POOSH' },{ 'fxn': fxns.chooz, 'text': 'CHOOZ' }
+
+    ],
+    'redirs': [ { 'text': 'LOGIN' }, { 'text': 'SIGN UP' }, { 'text': 'OPEN' }  ]
+};
