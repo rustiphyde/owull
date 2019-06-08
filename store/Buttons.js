b9e4b9cfd1e02760ea3bc12c3e2af02fed8a6678
Rusty Hoppins <rustiphyde@gmail.com>
2019-06-08 00:20:33 -0500

    Fix possible function error

--- store/Buttons.js
+++ store/Buttons.js
@@ -2,5 +2,5 @@
 export default {
     'fxnal': [ { 'text': 'LOGIN', 'id': 'btnLogin', 'class': 'btn' },{ 'text': 'REGISTER', 'id': 'btnRegister', 'class': 'btn' }, { 'text': 'RESET', 'id': 'btnReset', 'class': 'btn' },{ 'text': 'LOGOUT', 'id': 'btnLogout', 'class': 'btn' }
     ],
-    'redirs': [ { 'text': 'LOGIN', 'id': 'btnLogin', 'class': 'btn' }, { 'text': 'REGISTER', 'id': 'btnRegister', 'class': 'btn' }  ]
+    'redirs': [ { 'text': 'LOGIN', 'id': 'btnLog', 'class': 'btn' }, { 'text': 'REGISTER', 'id': 'btnRegist', 'class': 'btn' }  ]
 };
